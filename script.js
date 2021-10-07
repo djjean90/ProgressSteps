076bbb51453c4003248654e52fe246d59a1fd58d
MM work <djjean90@gmail.com>
2021-10-07 18:21:28 +0300

    final

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//Script.js
+
+const progress = document.querySelector('#progress'),
+      prev = document.querySelector('#prev'),
+      next = document.querySelector('#next'),
+      circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+function update() {
+        circles.forEach((circle, index) => {
+            if(index < currentActive) {
+                circle.classList.add('active');
+            } else {
+                circle.classList.remove('active');
+            }
+        });
+        const actives = document.querySelectorAll('.active'); 
+
+        //Procent calculating
+
+        progress.style.width = ((actives.length -1 )/ (circles.length -1)) * 100 +'%';
+    }
+
+next.addEventListener('click', ()=> {
+    currentActive++;
+    if(currentActive > circles.length){
+        currentActive = circles.length;
+    }
+    update();
+});
+
+prev.addEventListener('click', ()=> {
+    currentActive--;
+    if(currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+});
